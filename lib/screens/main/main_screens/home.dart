efd112e0068d23a019a1060062298d19c52b3764
Thaar-Abo-shah <thaaraboshah@gmail.com>
2022-10-27 17:37:53 +0400

    test map in home

--- lib/screens/main/main_screens/home.dart
+++ lib/screens/main/main_screens/home.dart
@@ -385,6 +385,7 @@ class _HomeState extends State<Home> {
                   child: currentLocation == null
                       ? const Center(child: Text("Loading"))
                       : GoogleMap(
+                          myLocationEnabled: true,
                           initialCameraPosition: CameraPosition(
                             target: LatLng(currentLocation!.latitude!,
                                 currentLocation!.longitude!),
